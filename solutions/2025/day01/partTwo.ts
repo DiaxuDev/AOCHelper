5da3381ed0d2bfc4cdbd28aee35fd93b3d495d38
DiaxuDev <diaxman.gameplay@gmail.com>
2025-12-02 18:34:24 +0100

    doc(aoc): add a comment to day one part two

--- solutions/2025/day01/partTwo.ts
+++ solutions/2025/day01/partTwo.ts
@@ -1,5 +1,12 @@
 import { run } from "aoc-framework";
 
+// WARNING! This script is broken as fuck and it's left here only for historical purposes.
+// This code incorrectly adds a point when the current was at 0 and moves to a value below 0 or above 99
+// I have no idea how I managed to get the right answer with this piece of shit
+// Maybe I'm just the chosen one
+// Anyhow I think that fixing it would be altering the history and I don't want to do that so I'm leaving it as is
+// Just be aware that you have maybe 1% chance or even lower getting the right answer with that
+
 run(
 	(input) => {
 		let current = 50;
